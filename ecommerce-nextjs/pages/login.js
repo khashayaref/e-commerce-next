bcb0ff797ee952656f30d97d8516a14461bef17f
khashayar <khashayaref2020@gmail.com>
2022-07-02 18:58:46 +0430

    create user token and write api for sending user data and checkthe authentication

--- ecommerce-nextjs/pages/login.js
+++ ecommerce-nextjs/pages/login.js
@@ -9,11 +9,27 @@ import {
 import Layout from '../components/layout/Layout';
 import classes from './login.module.css';
 import NextLink from 'next/link';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Login = ({}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('success login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -25,6 +41,8 @@ const Login = ({}) => {
               fullWidth
               label="Email"
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -34,6 +52,8 @@ const Login = ({}) => {
               fullWidth
               label="Password"
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
