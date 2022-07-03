d56f07f8d57559ded37a5890114ea50620609c6c
khashayar <khashayaref2020@gmail.com>
2022-07-03 22:47:31 +0430

    complete login and logout logic and set it to cookies and create menue in lauout navbar

--- ecommerce-nextjs/pages/login.js
+++ ecommerce-nextjs/pages/login.js
@@ -11,8 +11,23 @@ import classes from './login.module.css';
 import NextLink from 'next/link';
 import axios from 'axios';
 import { useState } from 'react';
+import { useContext } from 'react';
+import { Store } from '../utils/Store';
+import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
+import { useEffect } from 'react';
 
 const Login = ({}) => {
+  const router = useRouter();
+  const { redirect } = router.query;
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+  useEffect(() => {
+    if (userInfo) {
+      router.push('/');
+    }
+  }, []);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const submitHandler = async (e) => {
@@ -22,7 +37,9 @@ const Login = ({}) => {
         email,
         password,
       });
-      alert('success login');
+      dispatch({ type: 'USER_LOGIN', payload: data });
+      Cookies.set('userInfo', JSON.stringify(data));
+      router.push(redirect || '/');
     } catch (err) {
       alert(err.response.data ? err.response.data.message : err.message);
     }
